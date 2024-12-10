80a49d561b3f6400cd7d6503cf2f99234015af1d
RealVegas <grafit-design@inbox.ru>
2024-12-11 00:03:58 +0500

    Delete news_project directory

--- news_project/news/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class NewsPost(models.Model):
-    title = models.CharField('Название новости', max_length=100)
-    description = models.TextField('Краткое описание', max_length=200)
-    news_text = models.TextField('Текст новости')
-    news_date=models.DateTimeField('Дата публикации')
--- news_project/start.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-if __name__ == '__main__':
-    os.system('python news_project/manage.py runserver')
